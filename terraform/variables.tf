0d43634d85ed17b573d67747254b332a7f3d0d68
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2025-05-18 18:04:06 +0200

    style: apply terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,22 @@
-
 variable "project_id" {
-  type        = string
+  type = string
 }
 
 variable "region" {
-  type        = string
-  default     = "europe-west1"
+  type    = string
+  default = "europe-west1"
 }
 
 variable "enabled_apis" {
-  type        = list(string)
+  type = list(string)
   default = [
-    "compute.googleapis.com",           
-    "run.googleapis.com",               
-    "sqladmin.googleapis.com",          
-    "iam.googleapis.com",               
-    "secretmanager.googleapis.com",     
-    "serviceusage.googleapis.com",      
-    "logging.googleapis.com",           
-    "monitoring.googleapis.com"         
+    "compute.googleapis.com",
+    "run.googleapis.com",
+    "sqladmin.googleapis.com",
+    "iam.googleapis.com",
+    "secretmanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com"
   ]
 }
