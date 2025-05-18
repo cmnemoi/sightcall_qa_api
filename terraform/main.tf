834663c2e6b7ca76279b2f1289181188b6aa3826
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2025-05-18 19:21:46 +0200

    ci: Do not create GCP infra from scratch

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,25 +13,9 @@ terraform {
   }
 }
 
-resource "google_project" "project" {
-  name       = var.project_name
-  project_id = var.project_id
-}
-
 resource "google_project_service" "enabled" {
-  for_each           = toset(var.enabled_apis)
-  project            = google_project.project.project_id
-  service            = each.value
+  for_each = toset(var.enabled_apis)
+  project  = var.project_id
+  service  = each.value
   disable_on_destroy = false
-}
-
-resource "google_billing_account_iam_member" "billing_admin" {
-  billing_account_id = var.billing_account_id
-  role               = "roles/billing.admin"
-  member             = "user:${var.billing_user_email}"
-}
-
-resource "google_billing_project_info" "billing" {
-  project         = google_project.project.project_id
-  billing_account = var.billing_account_id
 }
