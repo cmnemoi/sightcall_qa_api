576e1d555386ff0063f02220a1cbc1d8b9361601
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2025-05-18 17:48:22 +0200

    ci: Add a GitHub workflow to setup project GCP infrastructure with Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+
+variable "project_id" {
+  type        = string
+}
+
+variable "region" {
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "enabled_apis" {
+  type        = list(string)
+  default = [
+    "compute.googleapis.com",           
+    "run.googleapis.com",               
+    "sqladmin.googleapis.com",          
+    "iam.googleapis.com",               
+    "secretmanager.googleapis.com",     
+    "serviceusage.googleapis.com",      
+    "logging.googleapis.com",           
+    "monitoring.googleapis.com"         
+  ]
+}
